57777e20cfca7640602ccf16b39287ebe6556c70
cs-fr <chuanshi_fr@163.com>
2019-12-04 15:40:41 +0800

    调整配置

---
+++ config.js
@@ -0,0 +1,19 @@
+/*
+* @Author: zhenghao01
+* @Date:   2019-12-04 14:29:51
+* @Last Modified by:   zhenghao01
+* @Last Modified time: 2019-12-04 15:40:10
+*/
+const promptList=[
+    {
+        type:'confirm',
+        name:'css',
+        message:'您是否使用SASS？'
+    },
+    {
+        type:'input',
+        name:'publicPath',
+        message:'请输入媒体服务器地址：'
+    }
+];
+module.exports=promptList;
