860348f5c06b7d394d0b677eede26b1238bf09f5
cs-fr <chuanshi_fr@163.com>
2019-12-05 10:20:54 +0800

    PC模板修改

--- config.js
+++ config.js
@@ -2,18 +2,33 @@
 * @Author: zhenghao01
 * @Date:   2019-12-04 14:29:51
 * @Last Modified by:   zhenghao01
-* @Last Modified time: 2019-12-04 15:40:10
+* @Last Modified time: 2019-12-05 10:19:49
 */
 const promptList=[
+    {
+        type:'input',
+        name:'author',
+        message:'请输入开发人员信息：'
+    },
+    {
+        type:'input',
+        name:'Desc',
+        message:'请输入项目描述：'
+    },
+    {
+        type:'input',
+        name:'publicPath',
+        message:'请输入媒体服务器地址：'
+    },
     {
         type:'confirm',
         name:'css',
         message:'您是否使用SASS？'
     },
     {
-        type:'input',
-        name:'publicPath',
-        message:'请输入媒体服务器地址：'
+        type:'confirm',
+        name:'eslint',
+        message:'您是否启用EsLint？'
     }
 ];
 module.exports=promptList;
